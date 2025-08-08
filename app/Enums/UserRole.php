e50b5c4b0b7c6c9da399d0774d3b45d290e41845
Shihab Mahamud <shihab4t@gmail.com>
2025-08-09 04:48:47 +0600

    author and admin

--- app/Enums/UserRole.php
+++ app/Enums/UserRole.php
@@ -10,5 +10,4 @@ enum UserRole: string
 
     case ADMIN = 'admin';
     case AUTHOR = 'author';
-    case SUBSCRIBER = 'subscriber';
 }
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->uuid('id')->primary();
 
-            $table->string('role')->default(UserRole::SUBSCRIBER->value);
+            $table->string('role')->default(UserRole::AUTHOR->value);
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
