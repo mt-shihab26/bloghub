f890bacf9f0833612e9f903fee2f84d0b8dc1e9f
Shihab Mahamud <shihab4t@gmail.com>
2025-08-09 00:57:21 +0600

    fixing test

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->uuid('id')->primary();
 
-            $table->strng('role')->default(Role::SUBSCRIBER->value);
+            $table->string('role')->default(Role::SUBSCRIBER->value);
 
             $table->string('name');
             $table->string('email')->unique();
