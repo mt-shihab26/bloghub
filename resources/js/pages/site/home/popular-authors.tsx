0cd4716704545797c4f8e10e34dc5dcd4dff75dc
Shihab Mahamud <shihab4t@gmail.com>
2025-08-09 13:22:43 +0600

    poplar authors

--- resources/js/pages/site/home/popular-authors.tsx
+++ resources/js/pages/site/home/popular-authors.tsx
@@ -17,16 +17,18 @@ export const PopularAuthors = ({ users }: { users: TUser[] }) => {
             <CardContent>
                 <div className="space-y-4">
                     {users.map((user) => (
-                        <div key={user.username} className="flex items-center space-x-3">
-                            <Avatar className="h-10 w-10">
-                                <AvatarImage src={imageLink(user.image)} />
-                                <AvatarFallback>{formatInitials(user.name)}</AvatarFallback>
-                            </Avatar>
-                            <div className="flex-1">
-                                <Link href={authorLink(user)} className="font-medium hover:underline">
-                                    {user.name}
-                                </Link>
-                                <p className="text-sm text-muted-foreground">@{user.username}</p>
+                        <div key={user.username} className="space-y-2">
+                            <div className="flex items-center space-x-3">
+                                <Avatar className="h-10 w-10">
+                                    <AvatarImage src={imageLink(user.image)} />
+                                    <AvatarFallback>{formatInitials(user.name)}</AvatarFallback>
+                                </Avatar>
+                                <div className="flex-1">
+                                    <Link href={authorLink(user)} className="font-medium hover:underline">
+                                        {user.name}
+                                    </Link>
+                                    <p className="text-sm text-muted-foreground">@{user.username}</p>
+                                </div>
                             </div>
                             <Button size="sm" variant="outline">
                                 Follow
