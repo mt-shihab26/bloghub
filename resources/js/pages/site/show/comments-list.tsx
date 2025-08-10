695e77a144419baf1b1f596ef53bbaf14270db92
Shihab Mahamud <shihab4t@gmail.com>
2025-08-11 00:28:00 +0600

    render comments

--- resources/js/pages/site/show/comments-list.tsx
+++ resources/js/pages/site/show/comments-list.tsx
@@ -1,19 +1,25 @@
-import type { TShowPost } from '@/types/site';
+import type { TShowComment, TShowPost } from '@/types/site';
 
 import { CommentSingle } from './comment-single';
+
+const RenderComments = ({ comment, depth }: { comment: TShowComment; depth: number }) => {
+    return (
+        <div className="space-y-4">
+            <CommentSingle comment={comment} />
+            <div style={{ marginLeft: depth * 24 }}>
+                {comment.comments?.map((comment) => (
+                    <RenderComments key={comment.id} comment={comment} depth={depth + 1} />
+                ))}
+            </div>
+        </div>
+    );
+};
 
 export const CommentsList = ({ post }: { post: TShowPost }) => {
     return (
         <div className="space-y-6">
             {post.comments?.map((comment) => (
-                <div key={comment.id} className="space-y-4">
-                    <CommentSingle comment={comment} />
-                    {comment.comments?.map((reply) => (
-                        <div key={reply.id} className="ml-12">
-                            <CommentSingle comment={reply} />
-                        </div>
-                    ))}
-                </div>
+                <RenderComments key={comment.id} comment={comment} depth={0} />
             ))}
         </div>
     );
