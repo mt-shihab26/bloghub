49e3a622ed26a6683431a6a588ba58ea16f6ab56
Shihab Mahamud <shihab4t@gmail.com>
2025-10-20 07:56:47 +0600

    search card

--- resources/js/components/screens/search/article-card.tsx
+++ resources/js/components/screens/search/article-card.tsx
@@ -12,58 +12,62 @@ import { Clock } from 'lucide-react';
 
 export const ArticleCard = ({ ith, post }: { ith: number; post: TSearchPost }) => {
     return (
-        <div key={post.id} className="flex flex-col overflow-hidden rounded-lg border p-4">
-            <div className="flex items-center justify-between">
-                <div className="flex items-center space-x-2">
-                    <Avatar className="size-10">
-                        <AvatarImage src={imageLink(post.user.image)} />
-                        <AvatarFallback>{formatInitials(post.user.name)}</AvatarFallback>
-                    </Avatar>
-                    <div className="flex flex-col">
-                        <Link href={authorLink(post.user)} className="text-sm font-medium hover:underline">
-                            {post.user.name}
-                        </Link>
-                        <span className="text-sm text-muted-foreground">
-                            {formatHumanDate(post.published_at)} ({formatTimeAgo(post.published_at)})
-                        </span>
-                    </div>
-                </div>
-                <Badge className="rounded-full" variant="outline">
-                    {ith}
-                </Badge>
+        <div key={post.id} className="flex space-x-2 overflow-hidden rounded-lg border p-4">
+            <div>
+                <Avatar className="size-10">
+                    <AvatarImage src={imageLink(post.user.image)} />
+                    <AvatarFallback>{formatInitials(post.user.name)}</AvatarFallback>
+                </Avatar>
             </div>
-            <h2 className="text-xl font-semibold">
-                <Link href={postLink(post.user, post)} className="hover:underline">
-                    {post.title}
-                </Link>
-            </h2>
-            <p className="text-muted-foreground">{post.excerpt}</p>
-            <div className="flex items-center justify-between space-x-2">
-                <div className="flex flex-wrap items-center space-x-2">
-                    {post.category && (
-                        <Link href={categoryLink(post.category)}>
-                            <Badge
-                                variant="outline"
-                                className="cursor-pointer hover:bg-primary hover:text-primary-foreground"
-                            >
-                                {categoryName(post.category)}
-                            </Badge>
-                        </Link>
-                    )}
-                    {post.tags?.map((tag) => (
-                        <Link key={tag.slug} href={tagLink(tag)}>
-                            <Badge variant="secondary" className="cursor-pointer text-xs hover:underline">
-                                #{tag.name}
-                            </Badge>
-                        </Link>
-                    ))}
-                </div>
-                <div className="flex flex-wrap items-center space-x-2">
-                    <div className="flex items-center space-x-1 text-sm text-muted-foreground">
-                        <Clock className="size-3" />
-                        <span>{readingTime(post.content)} min read</span>
+            <div>
+                <div className="flex items-center justify-between">
+                    <div className="flex items-center space-x-2">
+                        <div className="flex flex-col">
+                            <Link href={authorLink(post.user)} className="text-sm font-medium hover:underline">
+                                {post.user.name}
+                            </Link>
+                            <span className="text-sm text-muted-foreground">
+                                {formatHumanDate(post.published_at)} ({formatTimeAgo(post.published_at)})
+                            </span>
+                        </div>
                     </div>
-                    <BookmarkButton post={post} />
+                    <Badge className="rounded-full" variant="outline">
+                        {ith}
+                    </Badge>
+                </div>
+                <h2 className="text-xl font-semibold">
+                    <Link href={postLink(post.user, post)} className="hover:underline">
+                        {post.title}
+                    </Link>
+                </h2>
+                <p className="text-muted-foreground">{post.excerpt}</p>
+                <div className="flex items-center justify-between space-x-2">
+                    <div className="flex flex-wrap items-center space-x-2">
+                        {post.category && (
+                            <Link href={categoryLink(post.category)}>
+                                <Badge
+                                    variant="outline"
+                                    className="cursor-pointer hover:bg-primary hover:text-primary-foreground"
+                                >
+                                    {categoryName(post.category)}
+                                </Badge>
+                            </Link>
+                        )}
+                        {post.tags?.map((tag) => (
+                            <Link key={tag.slug} href={tagLink(tag)}>
+                                <Badge variant="secondary" className="cursor-pointer text-xs hover:underline">
+                                    #{tag.name}
+                                </Badge>
+                            </Link>
+                        ))}
+                    </div>
+                    <div className="flex flex-wrap items-center space-x-2">
+                        <div className="flex items-center space-x-1 text-sm text-muted-foreground">
+                            <Clock className="size-3" />
+                            <span>{readingTime(post.content)} min read</span>
+                        </div>
+                        <BookmarkButton post={post} />
+                    </div>
                 </div>
             </div>
         </div>
