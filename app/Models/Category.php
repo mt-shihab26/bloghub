03ed9b3670e367cc7eb9659e802f321163e8e66b
Shihab Mahamud <shihab4t@gmail.com>
2025-10-21 10:20:35 +0600

    add tosearchablearray method to category and tag models for scout integration

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,11 +7,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Laravel\Scout\Searchable;
 
 class Category extends Model
 {
     /** @use HasFactory<\Database\Factories\CategoryFactory> */
-    use HasFactory, HasUuids;
+    use HasFactory, HasUuids, Searchable;
 
     /**
      * The attributes that are mass assignable.
@@ -24,6 +25,22 @@ class Category extends Model
         'slug',
         'description',
     ];
+
+    /**
+     * Get the indexable data array for the model.
+     *
+     * @return array<string, mixed>
+     */
+    public function toSearchableArray(): array
+    {
+        return [
+            'id' => $this->id,
+            'slug' => $this->slug,
+            'name' => $this->name,
+            'description' => $this->description,
+            'created_at' => $this->created_at->timestamp,
+        ];
+    }
 
     /**
      * Get the user who owns/created this category.
