0d3cd2036c28dc986990c5409a1d86a8c29bd6c0
Shihab Mahamud <shihab4t@gmail.com>
2025-10-21 12:48:52 +0600

    disable @typescript-eslint/no-explicit-any rule

--- eslint.config.js
+++ eslint.config.js
@@ -22,6 +22,7 @@ export default [
             'react/prop-types': 'off',
             'react/no-unescaped-entities': 'off',
             '@typescript-eslint/no-unused-vars': 'off',
+            '@typescript-eslint/no-explicit-any': 'off',
         },
         settings: {
             react: {
