cddae68b3bdf078f7b634e4c8340b8f510875603
Shihab Mahamud <shihab4t@gmail.com>
2025-10-23 02:06:06 +0600

    theme

--- resources/js/layouts/site-layout/header-theme.tsx
+++ resources/js/layouts/site-layout/header-theme.tsx
@@ -1,13 +1,14 @@
 import { useAppearance } from '@/hooks/use-appearance';
 
 import { Button } from '@/components/ui/button';
-import { Moon, Sun } from 'lucide-react';
+import { Monitor, Moon, Sun } from 'lucide-react';
 
 export const HeaderTheme = () => {
     const { appearance, updateAppearance } = useAppearance();
 
     return (
         <Button variant="ghost" size="icon" onClick={() => updateAppearance(appearance === 'light' ? 'dark' : 'light')}>
+            {appearance === 'system' && <Monitor className="h-5 w-5" />}
             {appearance === 'light' && <Moon className="h-5 w-5" />}
             {appearance === 'dark' && <Sun className="h-5 w-5" />}
         </Button>
